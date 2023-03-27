dcff47f416f55d415ab2c636930435bfc9f52e3f
SubjectZero0 <iordanoualexander@gmail.com>
2023-03-27 16:25:33 +0300

    Creates Automapper Configurations

---
+++ Configurations/AutoMapperConfig.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CVApplicationsManager.Models;
+using CVApplicationsManager.Views;
+
+namespace CVApplicationsManager.Configurations
+{
+    public class AutoMapperConfig : Profile
+    {
+        protected internal AutoMapperConfig()
+        {
+            CreateMap<CvApplicationModel,CvApplicationViewModel>().ReverseMap();
+            CreateMap<DegreesModel, DegreesViewModel>().ReverseMap();
+        }
+    }
+}
