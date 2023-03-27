fb6a12a00fb8083b98deb9920eed6201ce768040
Alexandrer Iordanou <iordanoualexander@gmail.com>
2023-03-27 22:24:09 +0300

    Adds Migration

---
+++ Migrations/20230327164601_ChangeDegreeNameToDegree.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CVApplicationsManager.Migrations
+{
+    public partial class ChangeDegreeNameToDegree : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
