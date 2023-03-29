1c382d4949fd7c9f92ff2f174df33d998d6fcabe
SubjectZero0 <iordanoualexander@gmail.com>
2023-03-29 10:10:57 +0300

    Adds Migrations

---
+++ Migrations/20230329070519_SeedsDegrees.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CVApplicationsManager.Migrations
+{
+    public partial class SeedsDegrees : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Degrees",
+                columns: new[] { "Id", "DegreeName" },
+                values: new object[,]
+                {
+                    { 1, "Secondary" },
+                    { 2, "Bachelor's" },
+                    { 3, "Master's" },
+                    { 4, "PhD" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Degrees",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Degrees",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Degrees",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Degrees",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
