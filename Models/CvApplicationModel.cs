fcdbf1f0c8d41289ef5047910fcf9f3893c1f6e9
Alexandrer Iordanou <iordanoualexander@gmail.com>
2023-04-06 22:53:51 +0300

    Removes applicant model and Adds Maxlength Validation

--- Models/CvApplicationModel.cs
+++ Models/CvApplicationModel.cs
@@ -1,9 +1,30 @@
-﻿namespace CVApplicationsManager.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CVApplicationsManager.Models
 {
-    public class CvApplicationModel : ApplicantModel
+    public class CvApplicationModel
     {
         public int Id { get; set; }
 
+        [MaxLength(20)]
+        public string Firstname { get; set; }
+
+        [MaxLength(20)]
+        public string Lastname { get; set; }
+
+        [MaxLength(60)]
+        public string Email { get; set; }
+
+        [MaxLength(10)]
+        public string? Mobile { get; set; }
+
+        [ForeignKey("DegreeId")]
+        public DegreesModel? Degree { get; set; }
+
+        public int? DegreeId { get; set; }
+
+        [MaxLength(60)]
         public string? CvBlob { get; set; }
 
         public DateTime DateCreated { get; set; }
--- Models/DegreesModel.cs
+++ Models/DegreesModel.cs
@@ -1,9 +1,12 @@
-﻿namespace CVApplicationsManager.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CVApplicationsManager.Models
 {
     public class DegreesModel
     {
         public int Id { get; set; }
 
+        [MaxLength(20)]
         public string DegreeName { get; set; }
     }
 }
